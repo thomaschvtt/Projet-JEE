aeb7deb904f287038b02b2be4c82fbe368bb050b
ThomasC <tchvtt@gmail.com>
2024-10-27 16:54:34 +0100

    Sprint Boot Init

--- demo/src/main/java/com/Application.java
+++ demo/src/main/java/com/Application.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class Application {
     public static void main(String[] args) {
-      SpringApplication.run(DemoApplication.class, args);
+      SpringApplication.run(Application.class, args);
     }
-    @GetMapping("/hello")
+    @GetMapping("/Application")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
